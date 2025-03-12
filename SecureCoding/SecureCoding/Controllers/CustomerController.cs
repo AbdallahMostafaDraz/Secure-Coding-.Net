de1003a0ad2ff1fd6ef3977f0c47d1a8ff4ffaae
Abdallah Mostafa <abdallah.mostafa.draz@gmail.com>
2025-03-12 13:13:07 +0200

    1. Input Validation

---
+++ SecureCoding/SecureCoding/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecureCoding.Models;
+
+namespace SecureCoding.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private static List<Customer> customers = new();
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(customer);
+            }
+            customers.Add(customer);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SecureCoding/SecureCoding/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecureCoding.Models
+{
+    public class Customer
+    {
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$", ErrorMessage = "First Name Must Be Start With a Capital Letter!")]
+        [Display(Name = "First Name")]
+        [StringLength(25, MinimumLength = 3)]
+        public string FirstName { get; set; } = string.Empty;
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$", ErrorMessage = "Last Name Must Be Start With a Capital Letter!")]
+        [Display(Name = "Last Name")]
+        [Length(3, 25)]
+        public string LastName { get; set; } = string.Empty;
+        [AllowedValues("Male", "Female", ErrorMessage = "Only Male or Female Allowed!")]
+        public string Gender {  get; set; } = string.Empty;
+    }
+}
